a1aa70f7a5cfcc69c85e96b7edcb20bd15114cf5
Filip Bartodziej <filipbartodziej@gmail.com>
2020-03-29 10:53:35 +0200

    Put Point template definition to its header file

--- include/model/geometry/point.hpp
+++ include/model/geometry/point.hpp
@@ -16,6 +16,26 @@ struct Point
     
 };
 
+
+template <typename T>
+Point<T>::Point()
+{
+
+}
+
+template <typename T>
+Point<T>::Point(T x, T y) : x(x), y(y)
+{
+
+}
+
+
+template <typename T>
+Point<T>::~Point()
+{
+
+}
+
 }
 }
 }
