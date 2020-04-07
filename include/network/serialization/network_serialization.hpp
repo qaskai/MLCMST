1f71e9514845b69fac610c28d1493cfd03115132
Filip Bartodziej <filipbartodziej@gmail.com>
2020-04-07 13:17:46 +0200

    Restructure serialization files

--- include/network/serialization/network_serialization.hpp
+++ include/network/serialization/network_serialization.hpp
@@ -1,14 +1,28 @@
 #pragma once
 
 #include <istream>
+#include <ostream>
 
 #include <network/network.hpp>
+#include <util/serializer.hpp>
 #include <util/deserializer.hpp>
 
 
 namespace MLCMST {
 namespace network {
 namespace serialization {
+
+class NetworkSerializer : public util::Serializer<Network>
+{
+public:
+    NetworkSerializer();
+    ~NetworkSerializer();
+    
+    void serialize(const Network& network, std::ostream& stream);
+
+};
+
+
 
 class NetworkDeserializer : public util::Deserializer<Network>
 {
---
+++ src/network/serialization/network_serialization.cpp
@@ -0,0 +1,66 @@
+#include <network/serialization/network_serialization.hpp>
+
+#include <vector>
+
+namespace MLCMST {
+namespace network {
+namespace serialization {
+
+
+// *************** NetworkSerializer *************** //
+
+
+NetworkSerializer::NetworkSerializer()
+{
+
+}
+
+NetworkSerializer::~NetworkSerializer()
+{
+
+}
+
+void NetworkSerializer::serialize(const Network& network, std::ostream& stream)
+{
+    int N = network.getSize();
+    stream << N << std::endl;
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++) {
+            stream << network.edgeCost(i, j) << " ";
+        }
+        stream << std::endl;
+    }
+}
+
+
+
+
+// *************** NetworkDeserializer *************** //
+
+NetworkDeserializer::NetworkDeserializer()
+{
+
+}
+
+NetworkDeserializer::~NetworkDeserializer()
+{
+
+}
+
+Network NetworkDeserializer::deserialize(std::istream& stream)
+{
+    int N;
+    stream >> N;
+    std::vector<std::vector<double>> costs(N, std::vector<double>(N));
+    for (int i=0; i<N; i++) {
+        for (int j=0; j<N; j++) {
+            stream >> costs[i][j];
+        }
+    }
+
+    return Network(costs);
+}
+
+}
+}
+}
