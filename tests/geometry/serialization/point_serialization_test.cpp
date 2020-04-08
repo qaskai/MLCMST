3225cf99f06923c22a2fdda63d645e676352228b
Filip Bartodziej <filipbartodziej@gmail.com>
2020-04-08 14:01:23 +0200

    Add Point serialization tests

---
+++ tests/geometry/serialization/point_serialization_test.cpp
@@ -0,0 +1,29 @@
+#include <catch2/catch.hpp>
+
+#include <sstream>
+
+#include <geometry/point.hpp>
+#include <geometry/serialization/point_serialization.hpp>
+
+using namespace MLCMST::geometry;
+
+
+TEST_CASE( "Point serialization", "[geometry][serialization][point]")
+{
+    Point<int> p(1,2);
+    std::stringstream ss;
+
+    SECTION( "serialize" ) {
+        serialization::PointSerializer<int>().serialize(p, ss);
+        int x, y;
+        ss >> x >> y;
+        REQUIRE( x == 1 );
+        REQUIRE( y == 2 );
+    }
+    SECTION( "deserialize" ) {
+        ss << "1 2";
+        Point<int> q = serialization::PointDeserializer<int>().deserialize(ss);
+        REQUIRE( q.x == p.x );
+        REQUIRE( q.y == p.y );
+    }
+}
