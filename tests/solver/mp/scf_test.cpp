1036ac3fdfb82a0a159e7b8b360c76d1ec4af389
Filip Bartodziej <filipbartodziej@gmail.com>
2020-04-21 21:00:17 +0200

    Add SCF solver test

---
+++ tests/solver/mp/scf_test.cpp
@@ -0,0 +1,55 @@
+#include <catch2/catch.hpp>
+
+#include <network/mlcc_network.hpp>
+#include <network/mlcmst.hpp>
+#include <solver/mp/scf.hpp>
+
+using namespace MLCMST;
+using namespace MLCMST::network;
+
+TEST_CASE( "SCF functional test", "[solver][mp][scf]" )
+{
+    MLCCNetwork mlcc_network(
+        0,
+        std::vector<CapacitatedNetwork>{
+            CapacitatedNetwork(1, Network({
+                { 0, 1, 2 },
+                { 1, 0, 1 },
+                { 2, 1, 0 }
+            })),
+            CapacitatedNetwork(2, Network({
+                { 0, 1, 2 },
+                { 1, 0, 2 },
+                { 2, 2, 0 }
+            })),
+        },
+        {0, 1, 1}
+    );
+
+    using SCF = solver::mp::SCF;
+    SECTION( "exact solution" ) {
+        SCF scf(true);
+        std::vector<int> expected_parents { 0, 0, 1 };
+        std::vector<int> expected_levels { -1, 1, 0 };
+
+        SCF::Result result = scf.solve(mlcc_network);
+
+        REQUIRE( result.finished );
+        REQUIRE( result.lower_bound.value() == Approx(2).margin(0.0001) );
+        for (int i=0; i<mlcc_network.vertexCount(); i++) {
+            if (i == mlcc_network.center())
+                continue;
+            REQUIRE( result.mlcmst->parent(i) == expected_parents[i] );
+            REQUIRE( result.mlcmst->edgeLevel(i) == expected_levels[i] );
+        }
+    }
+    SECTION( "linear programming" ) {
+        SCF scf;
+
+        SCF::Result result = scf.solve(mlcc_network);
+
+        REQUIRE( result.finished );
+        REQUIRE( result.lower_bound.value() == Approx(1.5).margin(0.0001) );
+        REQUIRE( !result.mlcmst.has_value() );
+    }
+}
