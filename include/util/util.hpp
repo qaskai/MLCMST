2d95ea16447caf1607da57dbfb6a6ef34ded6034
Filip Bartodziej <filipbartodziej@gmail.com>
2020-04-25 11:35:36 +0200

    Add -Wall flag to project compilation

--- include/util/util.hpp
+++ include/util/util.hpp
@@ -9,18 +9,18 @@ namespace MLCMST::util {
 std::string read_stream(std::istream& stream);
 
 template <typename T>
-std::vector<std::vector<T>> break_up(int N, std::vector<T> vec);
+std::vector<std::vector<T>> break_up(unsigned int N, std::vector<T> vec);
 
 
 // template implementation
 template <typename T>
-std::vector<std::vector<T>> break_up(int N, std::vector<T> vec)
+std::vector<std::vector<T>> break_up(unsigned int N, std::vector<T> vec)
 {
     std::vector<std::vector<T>> out;
-    int idx = 0;
+    unsigned int idx = 0;
     while (idx < vec.size()) {
         out.emplace_back();
-        for (int i = 0; i < N && idx < vec.size(); i++) {
+        for (unsigned int i = 0; i < N && idx < vec.size(); i++) {
             out.back().push_back(vec[idx++]);
         }
     }
