f5fcd2e752a8f5a63be151a3f73a7d32b76a3e02
Filip Bartodziej <filipbartodziej@gmail.com>
2020-07-05 15:36:45 +0200

    Change ortools dep to stable branch

--- DownloadDependencies.cmake
+++ DownloadDependencies.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
         or-tools
         GIT_REPOSITORY "https://github.com/google/or-tools.git"
-        GIT_TAG master
+        GIT_TAG stable
 )
 FetchContent_MakeAvailable(or-tools)
 
