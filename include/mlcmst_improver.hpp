6e9ffee8e3a4eaae8ae5eb67a0b8de745d5c04c5
Filip Bartodziej <filipbartodziej@gmail.com>
2020-07-28 10:51:58 +0200

    Add MLCMST improver interface

---
+++ include/mlcmst_improver.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <network/mlcmst.hpp>
+#include <network/mlcc_network.hpp>
+
+namespace MLCMST {
+
+class MLCMST_Improver
+{
+public:
+    ~MLCMST_Improver();
+
+    virtual network::MLCMST improve(network::MLCMST mlcmst, const network::MLCCNetwork& mlcc_network) = 0;
+};
+
+}
---
+++ src/mlcmst_improver.cpp
@@ -0,0 +1,7 @@
+#include <mlcmst_improver.hpp>
+
+namespace MLCMST {
+
+MLCMST_Improver::~MLCMST_Improver() = default;
+
+}
